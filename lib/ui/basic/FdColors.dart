4b17d00b8883038e2ca57c328276c8a37a794649
shixin.chen <shixin.chen@huaban.com>
2018-03-06 15:01:12 +0800

    Add colors and themes for app

---
+++ lib/ui/basic/FdColors.dart
@@ -0,0 +1,16 @@
+import 'dart:ui';
+
+// Colors in this app.
+class FDColors {
+
+  // Official colors from dribbble
+  static const Color pink = const Color(0xFFea4c89);
+  static const Color charcoal = const Color(0xFF333333);
+  static const Color linkBlue = const Color(0xFF3a8bbb);
+  static const Color pro = const Color(0xFF66cccc);
+  static const Color jobsHiring = const Color(0xFF7bbb5e);
+  static const Color teams = const Color(0xFF00b6e3);
+  static const Color playbook = const Color(0xFF66cc99);
+  static const Color slate = const Color(0xFF9da3a5);
+
+}
---
+++ lib/ui/basic/FdThemeData.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'FdColors.dart';
+
+// App's theme data
+final ThemeData fdThemeData = new ThemeData(
+    primaryColor: FDColors.charcoal
+);
